f15228f47ac04282263ea1a2d9cdc3b629ed2034
NaokiTani1548 <naoki.1121.hit.and.run48@gmail.com>
2025-02-24 20:39:30 +0900

    bug fix in db_connection & adjustment css

--- chat_app_server.go
+++ chat_app_server.go
@@ -12,13 +12,41 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Chat struct {
+	Chat_id     int
+	Chat_detail string
+	Author_id   int
+}
+
 // HandlerFunc 型の関数を定義
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB_Connection, _ := sql.Open("mysql", os.Getenv("DB_ROLE")+":"+os.Getenv("DB_PASS")+"@/"+os.Getenv("DB_NAME"))
+	defer DB_Connection.Close()
+	cmd := `SELECT * FROM chat`
+	rows, err := DB_Connection.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer rows.Close()
+	var body []Chat
+	for rows.Next() {
+		var chat Chat
+		err := rows.Scan(&chat.Chat_id, &chat.Chat_detail, &chat.Author_id)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		body = append(body, chat)
+	}
+
 	t, err := template.ParseFiles("views/index.html")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	t.Execute(w, nil)
+	t.Execute(w, body)
 }
 func getPostChat(w http.ResponseWriter, r *http.Request) {
 	err := godotenv.Load()
@@ -39,17 +67,21 @@ func fuga(w http.ResponseWriter, r *http.Request) {
 }
 
 func initDB() {
-	var err error
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	DB_Connection, err := sql.Open("mysql", os.Getenv("DB_ROLE")+":"+os.Getenv("DB_PASS")+"@/"+os.Getenv("DB_NAME"))
 
 	if err != nil {
 		log.Fatalf("DB connection error: %v", err)
 	}
-
-	// 実際に接続できるかテスト
 	if err = DB_Connection.Ping(); err != nil {
 		log.Fatalf("DB ping error: %v", err)
 	}
+
+	cmd := `INSERT INTO author (author_id) VALUES (1);`
+	DB_Connection.Exec(cmd)
 }
 
 func main() {
